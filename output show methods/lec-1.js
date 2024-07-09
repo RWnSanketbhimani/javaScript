fcfe8c14bf639c009c5bc7b38762bb2c2596b3da
sanketbhimanii <sanketbhimani2710@gmail.com>
2024-07-09 13:42:16 +0530

    js

---
+++ output show methods/lec-1.js
@@ -0,0 +1,11 @@
+ // < -- document write method -->
+ document.write(" Jay Shree Ram !! ");
+
+ // < -- console.log method-- >
+ console.log(" jay Shree krishna ");
+
+ // <-- alert -->
+ alert(" you are codder ?? ");
+
+ // <-- getElimentByid -->
+ document.getElementById("methods").innerHTML = "kem chho developers !!"
